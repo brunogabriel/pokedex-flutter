c8043eebeb2fd0c480c923e41c68d2de7bd6d5dd
Bruno Gabriel dos Santos <bruno.gsantos89@gmail.com>
2023-08-01 01:03:52 -0300

    evolution #COMMENT * Evolution navigation

--- pokedex/lib/feature/evolution/presentation/widgets/pokemon_chart.dart
+++ pokedex/lib/feature/evolution/presentation/widgets/pokemon_chart.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:pokedex_flutter/design/constants/pokedex_theme_data.dart';
 import 'package:pokedex_flutter/shared/extensions/int_extensions.dart';
 import 'package:pokedex_flutter/shared/extensions/string_extensions.dart';
@@ -17,56 +18,61 @@ class PokemonChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
-
-    return Column(
-      children: [
-        Stack(
-          children: [
-            SizedBox(
-              width: 100,
-              height: 100,
-              child: ShaderMask(
-                shaderCallback: (bounds) => LinearGradient(
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
-                  colors: [
-                    ...List.generate(10, (index) => (index / 60) * 1.0)
-                        .map((e) => Colors.grey.withOpacity(e))
-                        .toList()
-                  ],
-                ).createShader(bounds),
-                child: Image.asset(
-                  'images/pokeball.png'.asset(),
-                  color: Colors.white,
-                  // color: Colors.white.withOpacity(0.1),
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 100,
-              height: 100,
-              child: Center(
-                child: SizedBox(
-                  width: 75,
-                  height: 75,
-                  child: CachedNetworkImage(
-                    imageUrl: number.thumbnailUrl,
+    return InkWell(
+      onTap: () => context.pushReplacementNamed(
+        'details',
+        pathParameters: {'id': number.toString()},
+      ),
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              SizedBox(
+                width: 100,
+                height: 100,
+                child: ShaderMask(
+                  shaderCallback: (bounds) => LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: [
+                      ...List.generate(10, (index) => (index / 60) * 1.0)
+                          .map((e) => Colors.grey.withOpacity(e))
+                          .toList()
+                    ],
+                  ).createShader(bounds),
+                  child: Image.asset(
+                    'images/pokeball.png'.asset(),
+                    color: Colors.white,
+                    // color: Colors.white.withOpacity(0.1),
                   ),
                 ),
               ),
-            )
-          ],
-        ),
-        Text(number.pokenumber,
-            style: textTheme.bodyMedium
-                ?.copyWith(color: PokedexThemeData.textGrey)),
-        Text(
-          name,
-          style: textTheme.titleMedium?.copyWith(
-            fontWeight: FontWeight.bold,
+              SizedBox(
+                width: 100,
+                height: 100,
+                child: Center(
+                  child: SizedBox(
+                    width: 75,
+                    height: 75,
+                    child: CachedNetworkImage(
+                      imageUrl: number.thumbnailUrl,
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
-        ),
-      ],
+          Text(number.pokenumber,
+              style: textTheme.bodyMedium
+                  ?.copyWith(color: PokedexThemeData.textGrey)),
+          Text(
+            name,
+            style: textTheme.titleMedium?.copyWith(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- pokedex/lib/feature/stats/presentation/widgets/stats_success.dart
+++ pokedex/lib/feature/stats/presentation/widgets/stats_success.dart
@@ -1,4 +1,3 @@
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pokedex/pokedex.dart';
